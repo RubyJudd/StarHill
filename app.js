76f09f44be4a48477fb3ba62212b3364ddfaa5b0
Ruby <ruby.rj@gmail.com>
2020-10-10 16:24:47 +0000

    mongodb connect line altered

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const indexRoutes = require("./routes/index");
 
 
 // mongoose.connect('mongodb://localhost:27017/yelp_camp_v2', {
-mongoose.connect('Ruby:Foocat#01>@cluster0.b8u7z.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('Ruby:Foocat#01>@cluster0.b8u7z.mongodb.net/yelp_camp_v2?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
