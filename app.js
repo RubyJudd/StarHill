75b20c1e39ecd87f114ce64cdae91c29e7e38921
Ruby <ruby.rj@gmail.com>
2020-12-14 17:13:02 +0000

    version1

--- app.js
+++ app.js
@@ -1,89 +1,50 @@
 const express = require("express");
 const app = express();
 const request = require("request");
-const bodyParser  = require("body-parser");
-const mongoose = require("mongoose");
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const methodOverrid = require("method-override");
-const flash       = require("connect-flash");
 
-
-const User = require("./models/user");
-const Campground = require("./models/campground");
-const Comment = require("./models/comment");
-const seedDB  = require("./seeds");
-
-// Requiring Routes
-const commentRoutes = require("./routes/comments");
-const campgroundRoutes = require("./routes/campgrounds");
-const indexRoutes = require("./routes/index");
-
-
-
-// mongoose.connect('mongodb://localhost:27017/yelp_camp_v2', {
-mongoose.connect('mongodb+srv://Ruby:silas@cluster0.b8u7z.mongodb.net/yelp_camp_v2?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DataBase'))
-.catch(error => console.log(error.message));
-
-app.use(bodyParser.urlencoded({extended : true})); // app.use should be used here
-// app.use(bodyParser.json())
 
 app.set("view engine",  "ejs");
 
 app.use(express.static(__dirname + "/public"));
-// the directory that the style script is running is 
+app.use(express.static(__dirname + "/img"));
 
-app.use(methodOverrid("_method"));
 
-app.use(flash());
+app.get("/", function(req, res){	
+		res.render("landing");
+});
 
-// PASSPORT CONFIGURATION
-app.use(require("express-session")({
-	secret: "Once again",
-	resave: false, 
-	saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// Create middleware that will be used on every route and then go to the next() code 
-// 
-// this will check for the id of current user, it will be empty if no one is logged in 
-// it pass the value of current user to every single route 
-app.use(function(req, res, next){
-   res.locals.currentUser = req.user;
-   res.locals.error = req.flash("error");
-   res.locals.success = req.flash("success");
-   next();
+app.get("/about", function(req, res){	
+		res.render("about");
 });
 
 
+app.get("/contact", function(req, res){	
+		res.render("contact");
+});
 
+app.get("/storytelling", function(req, res){	
+		res.render("storytelling");
+});
 
+app.get("/arttherapy", function(req, res){	
+		res.render("arttherapy");
+});
 
+app.get("/adoptees", function(req, res){	
+		res.render("adoptees");
+});
 
+app.get("/adoptiveparents", function(req, res){	
+		res.render("adoptiveparents");
+});
 
-
-
-app.use(indexRoutes);
-app.use("/campgrounds/:id/comments", commentRoutes);
-app.use("/campgrounds", campgroundRoutes);
-
-// this will append /campgrounds in front of all our campgroundRoutes. This groups routes into topics and allows you to write shorter route declarations
+app.get("/birthparents", function(req, res){	
+		res.render("birthparents");
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log("Server Has Started!");
+  console.log("Server Has Started");
 });
 
-// app.listen(3000, function(){
-//     console.log("Listening on PORT: 3000");
-// });  
+
